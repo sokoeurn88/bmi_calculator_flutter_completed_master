cf4d03405eda4742f0cebb7ef9e92df52de46193
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-11-08 09:21:46 -0600

    bmi calculator

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,30 @@
+import 'package:bmi_calculator_flutter_completed_master/constants.dart';
+import 'package:flutter/material.dart';
+// import 'package:bmi_calculator/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap, required this.buttonTitle});
+
+  final Function onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap(),
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
